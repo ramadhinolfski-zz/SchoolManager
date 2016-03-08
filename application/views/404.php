f812a124e48d2c3c7c19e85654b5555ce1a18912
ramadhinolfski <hanafiah.ramadhan@gmail.com>
2016-03-08 23:17:28 +0700

    Modifikasi Page 404

---
+++ application/views/404.php
@@ -0,0 +1,17 @@
+<?php include "layout/header.php"; ?>
+<title>Page not found</title>
+<div class="amber-200 bg-big">
+  <div class="text-center">
+    <h1 class="text-shadow no-margin text-white text-4x p-v-lg">
+      <span class="text-2x font-bold m-t-lg block">404</span>
+    </h1>
+    <h2 class="h1 m-v-lg text-black">WADAW	!!</h2>
+    <p class="h5 m-v-lg text-u-c font-bold text-black">Maaf, laman yang anda cari tidak ditemukan, lebih baik cari pasangan hidup saja!!</p>
+    <div class="p-v-lg">
+      <a ui-sref="app.dashboard" md-ink-ripple class="md-btn indigo md-raised p-h-lg">
+        <span class="text-white">Cari Jodoh</span>
+      </a>
+    </div>
+  </div>
+</div>
+<?php include "layout/footer.php"; ?>
