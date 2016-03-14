7b818c8618aaf5e0ac623fa7d9d7c1346ea6dc7d
Ramadhinolfski <hanafiah.ramadhan@gmail.com>
2016-03-14 10:27:58 +0700

    First set up versi 0.1.0

---
+++ application/controllers/Error404.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Error404 extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('404');
+	}
+
+}
+
+/* End of file Error404.php */
+/* Location: ./application/controllers/Error404.php */
