f852274fc3811e7133b10252bd048bfb9a4868ba
Ramadhinolfski <hanafiah.ramadhan@gmail.com>
2016-03-14 10:45:15 +0700

    Memisahkan tempalate dan pembuatan halaman 404

---
+++ application/views/layout/foot.php
@@ -0,0 +1,19 @@
+		<!-- BEGIN JAVASCRIPT -->
+		<script src="<?php echo base_url(); ?>/assets/js/libs/jquery/jquery-1.11.2.min.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/libs/jquery/jquery-migrate-1.2.1.min.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/libs/bootstrap/bootstrap.min.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/libs/spin.js/spin.min.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/libs/autosize/jquery.autosize.min.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/libs/nanoscroller/jquery.nanoscroller.min.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/core/source/App.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/core/source/AppNavigation.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/core/source/AppOffcanvas.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/core/source/AppCard.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/core/source/AppForm.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/core/source/AppNavSearch.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/core/source/AppVendor.js"></script>
+		<script src="<?php echo base_url(); ?>/assets/js/core/demo/Demo.js"></script>
+		<!-- END JAVASCRIPT -->
+
+	</body>
+</html>
---
+++ application/views/layout/head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Material Admin - 404 page</title>
+
+		<!-- BEGIN META -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="keywords" content="your,keywords">
+		<meta name="description" content="Short explanation about this website">
+		<!-- END META -->
+
+		<!-- BEGIN STYLESHEETS -->
+		<link href='<?php base_url(); ?>/css?family=Roboto:300italic,400italic,300,400,500,700,900' rel='stylesheet' type='text/css'/>
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/theme-default/bootstrap.css?1422792965" />
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/theme-default/materialadmin.css?1425466319" />
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/theme-default/font-awesome.min.css?1422529194" />
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/theme-default/material-design-iconic-font.min.css?1421434286" />
+		<!-- END STYLESHEETS -->
+
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="<?php echo base_url(); ?>/assets/js/libs/utils/html5shiv.js?1403934957"></script>
+		<script type="text/javascript" src="<?php echo base_url(); ?>/assets/js/libs/utils/respond.min.js?1403934956"></script>
+		<![endif]-->
+	</head>
+	<body class="menubar-hoverable header-fixed ">
